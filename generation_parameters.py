bff3c395a51022a90bbb07a17ad46ac526beef34
Adrian Roman <asroman@ucdavis.edu>
2023-05-02 23:21:46 -0700

    include fsd50k dataset setup utils, change path to datatsets in generation params

--- generation_parameters.py
+++ generation_parameters.py
@@ -6,15 +6,15 @@ def get_params(argv='1'):
     # ########### default parameters (NIGENS data) ##############
     params = dict(
         db_name = 'nigens',  # name of the audio dataset used for data generation
-        rirpath = '/scratch/asignal/krauseda/DCASE_data_generator/RIR_DB',   # path containing Room Impulse Responses (RIRs)
-        mixturepath = 'E:/DCASE2022/TAU_Spatial_RIR_Database_2021/Dataset-NIGENS',  # output path for the generated dataset
-        noisepath = '/scratch/asignal/krauseda/DCASE_data_generator/Noise_DB',  # path containing background noise recordings
+        rirpath = '/scratch/data/TAU-SRIR/TAU-SRIR_DB',   # path containing Room Impulse Responses (RIRs)
+        mixturepath = '/scratch/data/TAU_Spatial_RIR_Database_2021/Dataset-NIGENS',  # output path for the generated dataset
+        noisepath = '/scratch/data/TAU-SRIR/TAU-SNoise_DB',  # path containing background noise recordings
         nb_folds = 2,  # number of folds (default 2 - training and testing)
         rooms2fold = [[10, 6, 1, 4, 3, 8], # FOLD 1, rooms assigned to each fold (0's are ignored)
                       [9, 5, 2, 0, 0, 0]], # FOLD 2
-        db_path = 'E:/DCASE2022/TAU_Spatial_RIR_Database_2021/Code/NIGENS',  # path containing audio events to be utilized during data generation
+        db_path = '/scratch/data/TAU_Spatial_RIR_Database_2021/Dataset-NIGENS/NIGENS',  # path containing audio events to be utilized during data generation
         max_polyphony = 3,  # maximum number of overlapping sound events
-        active_classes = [0, 1, 2, 3, 5, 6, 8, 9, 10, 11, 12, 13],  # list of sound classes to be used for data generation
+        active_classes = [0, 1, 2, 3, 5, 6, 8, 9, 10, 11, 12],  # list of sound classes to be used for data generation
         nb_mixtures_per_fold = [900, 300], # if scalar, same number of mixtures for each fold
         mixture_duration = 60., #in seconds
         event_time_per_layer = 40., #in seconds (should be less than mixture_duration)
@@ -28,10 +28,13 @@ def get_params(argv='1'):
 
     elif argv == '2': ###### FSD50k DATA
         params['db_name'] = 'fsd50k'
-        params['db_path']= '/scratch/asignal/krauseda/DCASE_data_generator/Code/FSD50k'
-        params['mixturepath'] = '/scratch/asignal/krauseda/Data-FSD'
+        params['rirpath'] = '/scratch/data/TAU-SRIR/TAU-SRIR_DB'
+        params['mixturepath'] = '/scratch/data/FSD50K-DCASE-SYNTH'
+        params['noisepath'] = '/scratch/data/TAU-SRIR/TAU-SNoise_DB'
+        params['db_path'] = '/scratch/data/FSD50K/FSD50K_DCASE'
         params['active_classes'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         params['max_polyphony'] = 2
+        params['obj_path'] = 'dcase22_updated_obj_dist.pkl'
 
     elif argv == '3': ###### NIGENS interference data
         params['active_classes'] = [4, 7, 14] 
